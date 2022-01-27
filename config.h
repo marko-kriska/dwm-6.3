591108c1c10b009746d34d445500c6329a834f7d
Marko Kriška <marko.kriska@pm.me>
2022-01-27 23:17:20 +0100

    update

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char dmenufont[]       = "Terminus:weight=regular:size=10:antialias
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#005577";
 static const char col_cyan[]        = "#2257a0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
