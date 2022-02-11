de6df2f6e253db8bbb0baecb7c5f6cbbcd5b5718
Marko Kriška <marko.kriska@pm.me>
2022-02-11 14:31:51 +0100

    update

--- config.def.h
+++ config.def.h
@@ -4,8 +4,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:weighh=Regular:size=10:antialias=true:hinting=true" };
-static const char dmenufont[]       = "Terminus:weight=Regular:size=10:antialias=true:hinting=true";
+static const char *fonts[]          = { "Terminus:size=10" };
+static const char dmenufont[]       = "Terminus:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -18,8 +18,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-/*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
-static const char *tags[] = { "www", "term", "doc", "vid" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
