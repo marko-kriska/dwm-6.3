707ce7a6d54c32613b85c61bb252d2a18d7d7bc8
Marko Kriška <marko.kriska@pm.me>
2022-03-09 20:54:44 +0100

    update

--- config.def.h
+++ config.def.h
@@ -18,7 +18,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/* static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
+static const char *tags[] = { "web", "term", "doc", "vid" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,7 +28,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+      /*{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },*/
+	{ "firefox",  NULL,       NULL,       1,       0,           -1 },
 };
 
 /* layout(s) */
