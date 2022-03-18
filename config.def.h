4940a58a4605f107ed8dbfa7c8ca5edb8b757929
Marko Kriška <marko.kriska@pm.me>
2022-03-18 12:12:41 +0100

    update

--- config.def.h
+++ config.def.h
@@ -1,11 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:size=10" };
-static const char dmenufont[]       = "Terminus:size=10";
+static const char *fonts[]          = { "Hermit:size=10:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Hermit:size=10:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
