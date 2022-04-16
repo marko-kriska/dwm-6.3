9eea23c90d30947cd1404e11a6aaeca3a61a8c21
Marko Kriška <marko.kriska@pm.me>
2022-04-16 19:48:51 +0200

    update

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hermit:weight=regular:size=10:antialias=true:hinting=true", 
+static const char *fonts[]          = { "FiraCode Nerd Font Mono:weight=regular:size=10:antialias=true:hinting=true", 
 					"JoyPixels:weight=regular:size=10:antialias=true:hinting=true" };
 static const char dmenufont[]       = "Hermit:weight=regular:size=10:antialias=true:hinting=true";
 static const char col_gray1[]       = "#222222";
