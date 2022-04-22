6ca1e36cb28398ddaac0a7443cf6ca93edab9fb6
Marko Kriška <marko.kriska@pm.me>
2022-04-23 01:44:51 +0200

    update

--- config.h
+++ config.h
@@ -5,9 +5,9 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fantasque Sans Mono:weight=regular:size=12:antialias=true:hinting=true", 
-					"JoyPixels:weight=regular:size=12:antialias=true:hinting=true" };
-static const char dmenufont[]       = "Fantasque Sans Mono:weight=regular:size=12:antialias=true:hinting=true";
+static const char *fonts[]          = { "Fantasque Sans Mono:weight=regular:size=14:antialias=true:hinting=true", 
+					"JoyPixels:weight=regular:size=14:antialias=true:hinting=true" };
+static const char dmenufont[]       = "Fantasque Sans Mono:weight=regular:size=14:antialias=true:hinting=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
