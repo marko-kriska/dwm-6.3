bb5a33fc359fb14129959d7a8b4ab5ab9b02780e
Marko Kriška <marko.kriska@pm.me>
2022-06-28 18:44:28 +0200

    update

--- config.def.h
+++ config.def.h
@@ -5,9 +5,9 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono Nerd Font:weight=regular:size=10:antialias=true:hinting=true", 
+static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:weight=regular:size=10:antialias=true:hinting=true", 
 					"JoyPixels:weight=regular:size=10:antialias=true:hinting=true" };
-static const char dmenufont[]       = "JetBrains Mono Nerd Font:weight=regular:size=10:antialias=true:hinting=true";
+static const char dmenufont[]       = "JetBrainsMonoMedium Nerd Font:weight=regular:size=10:antialias=true:hinting=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
