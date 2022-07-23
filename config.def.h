58d5a29aba26b2036d4104cb3916df0395c6377d
Marko Kriška <marko.kriska@pm.me>
2022-07-23 16:07:23 +0200

    update

--- config.def.h
+++ config.def.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "/bin/sh", "-c", "alacritty -e fish", NULL };
-static const char *filecmd[]    = { "pcmanfm",   NULL };
+static const char *filecmd[]    = { "nemo",   NULL };
 static const char *browsercmd[] = { "firefox",   NULL };
 static const char *screenshot[] = { "flameshot", "gui", NULL };
 
