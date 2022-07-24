95c3ff1b2abe10ecb7547a3e322795b6c957adab
Marko Kriška <marko.kriska@pm.me>
2022-07-24 14:59:48 +0200

    update

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#6272a4";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]    = { "/bin/sh", "-c", "alacritty -e fish", NULL };
-static const char *filecmd[]    = { "nemo",   NULL };
+static const char *filecmd[]    = { "pcmanfm",   NULL };
 static const char *browsercmd[] = { "firefox",   NULL };
 static const char *screenshot[] = { "flameshot", "gui", NULL };
 
