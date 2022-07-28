387ac507904ee1f8ff7384fc18db6261331227f2
Marko Kriška <marko.kriska@pm.me>
2022-07-28 17:36:09 +0200

    update

--- config.def.h
+++ config.def.h
@@ -9,14 +9,14 @@ static const char *fonts[]          = { "JetBrainsMonoMedium Nerd Font:weight=re
 					"JoyPixels:weight=regular:size=10:antialias=true:hinting=true" };
 static const char dmenufont[]       = "JetBrainsMonoMedium Nerd Font:weight=regular:size=10:antialias=true:hinting=true";
 static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#44475a";
+static const char col_gray2[]       = "#ffb86c";
 static const char col_gray3[]       = "#ff79c6";
-static const char col_gray4[]       = "#ff79c6";
-static const char col_cyan[]        = "#8be9fd";
+static const char col_gray4[]       = "#ffb86c";
+static const char col_cyan[]        = "#282a36";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    /*               fg         bg         border   */
+    [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
+    [SchemeSel]  = { col_gray4, col_cyan,  col_gray2  },
 };
 
 /* tagging */
