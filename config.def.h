2fe38b2f5b4ceaae004a2ff258cec3fa5beb91c8
Marko Kriška <marko.kriska@pm.me>
2022-07-30 16:49:52 +0200

    update

--- config.def.h
+++ config.def.h
@@ -62,6 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray1, "-sf", col_cyan, NULL };
 static const char *termcmd[]    = { "/bin/sh", "-c", "alacritty -e fish", NULL };
+static const char *roficmd[]    = { "/bin/sh", "-c", "alacritty -e rofi -show drun", NULL };
 static const char *filecmd[]    = { "pcmanfm",   NULL };
 static const char *browsercmd[] = { "firefox",   NULL };
 static const char *screenshot[] = { "flameshot", "gui", NULL };
@@ -78,7 +79,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	/* MODKEY */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd} },	
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_Print,  spawn,          {.v = screenshot} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
