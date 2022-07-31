5437760ee43e076a3928cad9f3a065563aab4ded
Marko Kriška <marko.kriska@pm.me>
2022-07-31 20:14:58 +0200

    update

--- config.def.h
+++ config.def.h
@@ -60,7 +60,6 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_gray1, "-sf", col_cyan, NULL };
-static const char *roficmd[]    = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]    = { "/bin/sh", "-c", "alacritty -e fish", NULL };
 static const char *filecmd[]    = { "pcmanfm",   NULL };
 static const char *browsercmd[] = { "firefox",   NULL };
@@ -77,7 +76,7 @@ static const char *brightdown[] = { "xbacklight", "-dec", "10", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	/* MODKEY */
-	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd} },	
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd} },	
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_Print,  spawn,          {.v = screenshot} },
